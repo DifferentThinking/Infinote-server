0f3d3dc7fafa1b266317bb875fc99b6cae6bd6b4
prot3ct <djorkaevhell@abv.bg>
2017-04-04 14:58:01 +0300

    Init

---
+++ app.js
@@ -0,0 +1,75 @@
+'use strict';
+
+// MongoDB config
+const mongojs = require('mongojs');
+const connectionString = 'mongodb://<dbuser>:<dbpassword>@ds135800.mlab.com:35800/infinote-db';
+const collections = ['users'];
+
+const db = mongojs(connectionString, collections);
+
+var ObjectId = require('mongodb').ObjectId; 
+
+const express = require('express')
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const apiRouter = new express.Router();
+
+apiRouter
+	.post('/auth/register', function (req, res, next) {
+		let user = req.body;
+		if (!user) {
+			return res.status(404).json({ "error": "Please Enter username or password" });
+		}
+		else {
+			db['users'].findOne({ username: req.body.username }, function(err, userInDb) {
+				if (userInDb) {
+					return res.status(404).json({"error": "Please choose another username. This username is already in use!"});
+				}
+
+				user.friends = ["no friends"];
+				db['users'].save(user, function (err, user) {
+					if (err) {
+						return res.status(404).json({"error": "DB error"});
+					}
+					return res.json({
+						username: user.username
+					});
+				})
+			})
+		}
+	})
+	.post('/auth/login', function(req, res, next)  {
+		if (!req.body) {
+			return res.status(404).json({ "error": "You must send the username and the password" });
+		}
+		db['users'].findOne({ username: req.body.username }, function(err, user) {
+			if (!user) {
+				return res.status(404).json({ "error": "The username or password doesn't match" });
+			}
+			if (!(user.passHash === req.body.passHash)) {
+				return res.status(404).json({ "error": "The username or password doesn't match" });
+			}
+			
+			res.json({
+				username: user.username
+			});
+		});
+	})
+	.get('/users', function (req, res, next) {
+		db['users'].find(function (err, users) {
+			if (err) {
+				return res.json({ "error": "DB Error"});
+			}
+			return res.json(users);
+		})
+	})
+app.use('/api', apiRouter);
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`Server running on port:${port}`);
