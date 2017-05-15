4a2815cf010f5b36545cbe688a30f009342a4786
Georgi Karaboichev <djorkaevhell@abv.bg>
2017-05-16 02:40:50 +0300

    Add route for single user

--- app.js
+++ app.js
@@ -23,17 +23,17 @@ apiRouter
 	.post('/auth/register', function (req, res, next) {
 		let user = req.body;
 		if (!user) {
-			return res.status(404).json({ "error": "Please Enter username or password" });
+			return res.status(404).json({ 'error': 'Please Enter username or password' });
 		}
 		else {
 			db['users'].findOne({ username: req.body.username }, function(err, userInDb) {
 				if (userInDb) {
-					return res.status(404).json({"error": "Please choose another username. This username is already in use!"});
+					return res.status(404).json({'error': 'Please choose another username. This username is already in use!'});
 				}
 
 				db['users'].save(user, function (err, user) {
 					if (err) {
-						return res.status(404).json({"error": "DB error"});
+						return res.status(404).json({'error': 'DB error'});
 					}
 					return res.json({ result: {
 						username: user.username
@@ -44,14 +44,14 @@ apiRouter
 	})
 	.post('/auth/login', function(req, res, next)  {
 		if (!req.body) {
-			return res.status(404).json({ "error": "You must send the username and the password" });
+			return res.status(404).json({ 'error': 'You must send the username and the password' });
 		}
 		db['users'].findOne({ username: req.body.username }, function(err, user) {
 			if (!user) {
-				return res.status(404).json({ "error": "The username or password doesn't match" });
+				return res.status(404).json({ 'error': 'The username or password doesn\'t match' });
 			}
 			if (!(user.passHash === req.body.passHash)) {
-				return res.status(404).json({ "error": "The username or password doesn't match" });
+				return res.status(404).json({ 'error': 'The username or password doesn\'t match' });
 			}
 			
 			res.json({ result: {
@@ -61,17 +61,17 @@ apiRouter
 	})
 	.post('/users/:username/notes', function(req, res, next) {
 		if (!req.body) {
-			return res.status(404).json({ "error": "You must send the username and the password" });
+			return res.status(404).json({ 'error': 'You must send the username and the password' });
 		}
 		let note = req.body;
 
 		db['note'].save(note, function(err, noteInDb) {
 			if (err) {
-				return res.status(404).json({ "error": "DB Error"});
+				return res.status(404).json({ 'error': 'DB Error'});
 			}
 			
-			return res.json({ "result": {
-				"status": "OK"
+			return res.json({ 'result': {
+				'status': 'OK'
 			}});
 		});
 	})
@@ -80,11 +80,11 @@ apiRouter
 
 		db['note'].remove({ _id: new ObjectId(id) }, function(err, deletedNote) {
 			if (err) {
-				return res.status(404).json({ "error": "Error in DB" });
+				return res.status(404).json({ 'error': 'Error in DB' });
 			}
 
-			return res.json({ "result": {
-				"status": "OK"
+			return res.json({ 'result': {
+				'status': 'OK'
 			}});
 		})
 	})
@@ -93,22 +93,29 @@ apiRouter
 		
 		db['note'].find({ username: username }, function(err, notes) {
 			if (err) {
-				return res.status(404).json({ "error": "DB Error"});
+				return res.status(404).json({ 'error': 'DB Error'});
 			}
 
-			return res.json({"result": {
-				"notes": notes
+			return res.json({'result': {
+				'notes': notes
 			}});
 		});
 	})
-	.get('/users', function (req, res, next) {
-		db['users'].find(function (err, users) {
+	.get('/users/:username', function (req, res, next) {
+		let username = req.params.username;
+
+		db['users'].find({ username: username }, function (err, user) {
 			if (err) {
-				return res.json({ "error": "DB Error"});
+				return res.json({ 'error': 'DB Error'});
 			}
-			return res.json(users);
+
+			user.remove('passHash')
+			return res.json({ result: {
+				user
+			}});
 		})
 	});
+	
 app.use('/api', apiRouter);
 
 const port = process.env.PORT || 3000;
